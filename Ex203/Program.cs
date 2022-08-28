0fc2bb92c462ea82e69008c0dd410152dc767df3
Dmitriy <dsmurov@mail.ru>
2022-08-28 13:44:43 +0300

    ex200

---
+++ Ex203/Program.cs
@@ -0,0 +1,28 @@
+﻿int day;
+         Console.Write("\n\n");
+Console.Write("Ведите день : ");
+         day = Convert.ToInt32(Console.ReadLine());
+   switch(day)
+   {
+	case 1:
+	       Console.Write("Нет рабочий день\n");
+	       break;
+	case 2:
+	       Console.Write("Нет рабочий день\n");
+	       break;
+	case 3:
+	       Console.Write("Нет рабочий день\n");
+	       break;
+	case 4:
+	       Console.Write("Нет рабочий день\n");
+	       break;
+	case 5:
+	       Console.Write("Нет рабочий день\n");
+	       break;
+	case 6:
+	       Console.Write("Да выходной день\n");
+	       break;
+	case 7:
+	       Console.Write("Да выходной день\n");
+	       break;
+	      }
